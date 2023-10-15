cf6135720bf80de8702eea7adc3d422594606d0e
Semih <ibrahim.semih@gmail.com>
2023-10-15 23:56:25 +0300

    remove unnecessary constants

--- document-save/src/main/java/com/food/recipe/api/DocumentConstant.java
+++ document-save/src/main/java/com/food/recipe/api/DocumentConstant.java
@@ -10,12 +10,6 @@ public class DocumentConstant {
 
     public static class Exception {
 
-        public static final String AUTH_USER_EXIST = "authentication.user_exits";
-
-        public static final String AUTH_EMAIL_EXIST = "authentication.email_exits";
-
-        public static final String AUTH_DASHBOARD_REJECTED = "authentication.admin_rejected";
-
         public static final String FILE_NOT_EMPTY = "file.not_empty";
 
         public static final String FILE_NOT_ALLOWED = "file.not_allowed";

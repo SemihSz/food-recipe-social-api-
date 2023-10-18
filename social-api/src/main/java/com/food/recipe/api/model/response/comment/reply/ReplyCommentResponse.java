8ce8a007b05d53fd268824de3550c441bbc9e651
Semih <ibrahim.semih@gmail.com>
2023-10-18 23:35:44 +0300

    java doc and comments add

--- social-api/src/main/java/com/food/recipe/api/model/response/comment/reply/ReplyCommentResponse.java
+++ social-api/src/main/java/com/food/recipe/api/model/response/comment/reply/ReplyCommentResponse.java
@@ -1,9 +1,16 @@
 package com.food.recipe.api.model.response.comment.reply;
 
-import com.food.recipe.api.model.response.comment.CommentResponse;
-import lombok.experimental.SuperBuilder;
+import lombok.Builder;
+import lombok.Data;
 
-@SuperBuilder
-public class ReplyCommentResponse extends CommentResponse {
+@Data
+@Builder
+public class ReplyCommentResponse {
+
+    private Long commentId;
+
+    private boolean isDeleted;
+
+    private boolean isUpdated;
 
 }

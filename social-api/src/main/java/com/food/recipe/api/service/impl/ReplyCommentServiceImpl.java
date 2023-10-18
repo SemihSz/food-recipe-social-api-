f24ddb5e71df6260109c087d93f7fba5442d786f
Semih <ibrahim.semih@gmail.com>
2023-10-18 23:31:21 +0300

    comment code

--- social-api/src/main/java/com/food/recipe/api/service/impl/ReplyCommentServiceImpl.java
+++ social-api/src/main/java/com/food/recipe/api/service/impl/ReplyCommentServiceImpl.java
@@ -31,28 +31,44 @@ public class ReplyCommentServiceImpl implements ReplyCommentService {
 
   private final AddReplyCommentService addReplyCommentService;
 
-  @Override
-  public ReplyCommentResponse addComment(BaseReplyCommentRequest request) {
 
-    final SocialUserEntity socialUserEntity = getSocialAppUserInfoService.apply(request.getUserId(), request.getUsername());
-    final PostEntity getPostInformation = getPostInformationService.apply(request.getPostId());
-    final CommentEntity comment = getCommentInformationService.apply(request.getCommentId());
+/**
+ * Adds a reply comment to a post based on the provided request.
+ *
+ * @param request The request containing the necessary information to add the reply comment.
+ * @return The response indicating the result of adding the reply comment.
+ *         Returns null if any of the required information is missing or invalid.
+ */
+@Override
+public ReplyCommentResponse addComment(BaseReplyCommentRequest request) {
 
-    if (Objects.nonNull(comment) && Objects.nonNull(getPostInformation) &&
-        Objects.nonNull(socialUserEntity) && !StringUtils.isEmpty(request.getDescription())) {
+  // Retrieve the social user information based on the user ID and username from the request
+  final SocialUserEntity socialUserEntity = getSocialAppUserInfoService.apply(request.getUserId(), request.getUsername());
 
-      final AddReplyCommentInput input = AddReplyCommentInput.builder()
-          .comment(comment)
-          .post(getPostInformation)
-          .socialUserInfo(socialUserEntity)
-          .replyCommentBody(request.getDescription())
-          .build();
+  // Retrieve the post information based on the post ID from the request
+  final PostEntity getPostInformation = getPostInformationService.apply(request.getPostId());
 
-      return addReplyCommentService.apply(input);
-    }
+  // Retrieve the comment information based on the comment ID from the request
+  final CommentEntity comment = getCommentInformationService.apply(request.getCommentId());
 
-    return null;
+  // Check if all the required information is available and the description is not empty
+  if (Objects.nonNull(comment) && Objects.nonNull(getPostInformation) &&
+      Objects.nonNull(socialUserEntity) && !StringUtils.isEmpty(request.getDescription())) {
+    // Create an input object for adding the reply comment
+    final AddReplyCommentInput input = AddReplyCommentInput.builder()
+        .comment(comment)
+        .post(getPostInformation)
+        .socialUserInfo(socialUserEntity)
+        .replyCommentBody(request.getDescription())
+        .build();
+
+    // Add the reply comment using the input object and return the response
+    return addReplyCommentService.apply(input);
   }
+
+  // Return null if any of the required information is missing or invalid
+  return null;
+}
 
   @Override
   public ReplyCommentResponse updateComment(UpdateReplyCommentRequest request) {

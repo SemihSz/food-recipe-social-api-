e5a6dc9e1259038a5b3d9f79cda3ba207c515dff
Semih <ibrahim.semih@gmail.com>
2023-10-18 23:18:56 +0300

    logger field created

--- social-api/src/main/java/com/food/recipe/api/config/LoggingFilterBean.java
+++ social-api/src/main/java/com/food/recipe/api/config/LoggingFilterBean.java
@@ -20,7 +20,7 @@ import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.Collection;
+import java.util.*;
 import java.util.function.Function;
 
 import static java.util.Collections.list;
@@ -51,6 +51,25 @@ public class LoggingFilterBean extends OncePerRequestFilter {
                 request.getCharacterEncoding());
         String responseBody = getStringValue(responseWrapper.getContentAsByteArray(),
                 response.getCharacterEncoding());
+
+        // HTTP başlıkları
+        Enumeration<String> headerNames = request.getHeaderNames();
+        Map<String, String> headerInfoRequest = new HashMap<>();
+        List<String> headerInfoResponse = new ArrayList<>();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            headerInfoRequest.put(headerName, headerValue);
+            LOGGER.info("Request Header: {} = {}", headerName, headerValue);
+        }
+
+        // Yanıt başlıkları
+        Collection<String> responseHeaderNames = responseWrapper.getHeaderNames();
+        for (String headerName : responseHeaderNames) {
+            String headerValue = responseWrapper.getHeader(headerName);
+            headerInfoResponse.add(headerValue);
+            LOGGER.info("Response Header: {} = {}", headerName, headerValue);
+        }
 
         LOGGER.info(
                 "FINISHED PROCESSING : METHOD={}; REQUEST_URI={};  REQUEST PAYLOAD={}; RESPONSE CODE={}; RESPONSE={}; TIM TAKEN={}",

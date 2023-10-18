51af7e87a1ab29e5db59cd3942231280f3259a7d
semih.sozdinler <semih.sozdinler@etstur.com>
2023-10-18 12:17:31 +0300

    bean injection warn fixed

--- authentication/src/main/java/com/food/recipe/api/controller/UserSummaryController.java
+++ authentication/src/main/java/com/food/recipe/api/controller/UserSummaryController.java
@@ -4,6 +4,7 @@ import com.food.recipe.api.model.RestResponse;
 import com.food.recipe.api.model.response.summary.UserSummaryResponse;
 import com.food.recipe.api.service.summary.UserSummaryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +17,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/summary")
 public class UserSummaryController {
 
-    private final UserSummaryService summaryService;
+    private final UserSummaryService userSummaryService;
 
     @GetMapping("/info/{username}")
     public ResponseEntity<RestResponse<UserSummaryResponse>> documentListl(@PathVariable String username) {
 
-        return ResponseEntity.ok(new RestResponse<>(HttpStatus.OK, summaryService.summary(username)));
+        return ResponseEntity.ok(new RestResponse<>(HttpStatus.OK, userSummaryService.summary(username)));
     }
 
     @GetMapping("/info")
     public ResponseEntity<RestResponse<UserSummaryResponse>> documentList(@RequestHeader(value = "username") String username) {
 
-        return ResponseEntity.ok(new RestResponse<>(HttpStatus.OK, summaryService.summary(username)));
+        return ResponseEntity.ok(new RestResponse<>(HttpStatus.OK, userSummaryService.summary(username)));
     }
 }
